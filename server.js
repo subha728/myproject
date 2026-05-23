98b4d064709700209af8945b901157e3db30f7ec
subha <mesubha.7@gmail.com>
2026-05-24 01:40:54 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const app = express();
+
+mongoose.connect("mongodb+srv://subha:durga321@cluster0.pm2tfls.mongodb.net/test?retryWrites=true&w=majority")
+.then(() => console.log("MongoDB Connected"))
+.catch((error) => console.log(error));
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Backend is working");
+});
+
+app.post("/contact", (req, res) => {
+    console.log(req.body);
+    res.send("Message received successfully");
+});
+
+app.listen(5000, () => {
+    console.log("Server running on port 5000");
+});
